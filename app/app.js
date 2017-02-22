b0551e7fc86f5742902be4270e22120de6c16f65
mayconds000 <mayconds000@gmail.com>
2017-02-21 22:03:45 -0300

    learning routes

--- app/app.js
+++ app/app.js
@@ -3,7 +3,9 @@ import Chalk from 'chalk'
 import Router from 'koa-router'
 
 const koa  = new Koa()
-const router = new Router()
+const router = new Router({
+  prefix: '/hello'
+})
 
 koa.use(function *(next) {
   yield next
@@ -12,11 +14,22 @@ koa.use(function *(next) {
 
 })
 
-router.get('/', function *() {
-  this.body = 'Hello from here'
-})
+router
+  .get('/', function *() {
+    this.body = 'Hello from here'
+  })
+  .post('main', '/hello', function *(next) {
+    this.body = 'This is my main router'
+    yield next
+  }, function *(next) {
+    console.log('my other function')
+  })
+  .put('main_put', '/put', function *() {
+    this.body = 'this is my put router'
+  })
 
 koa.use(router.routes())
+koa.use(router.allowedMethods())
 koa.listen(3000, () => {
   console.log('This server was listen the port 3000')
 })
